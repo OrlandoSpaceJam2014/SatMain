0b8fbced6586c19d6911396bad775631a5cde0bb
gamingod4u <mcman407@gmail.com>
2014-07-26 16:14:52 -0400

    Update

--- Assets/DialNeedleScript.cs
+++ Assets/DialNeedleScript.cs
@@ -7,25 +7,52 @@ public class DialNeedleScript : MonoBehaviour {
 	public float dialCurloc;
 	public float dialEndloc;
 	public float changeRate;
-
+	public float usageRate;
+	public bool  isMoving = false;
+	private bool isFull = false;
+	
 	// Use this for initialization
 	void Start () {
 		dialCurloc = gameObject.transform.rotation.y;
-		dialStartloc = 75.0f;
-		dialEndloc = 285.0f;
+		dialStartloc = 285.0f;
+		dialEndloc = 85.0f;
 		changeRate = 50;
-
+		usageRate = 0f;
+		
 		//Start the dial's current location at the dial's starting position.
-		dialCurloc = dialStartloc;
+		dialCurloc = dialEndloc;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		print (this.transform.localEulerAngles.y);
-		if (this.transform.localEulerAngles.y < dialEndloc) {
-			transform.Rotate (Vector3.up * -changeRate * Time.deltaTime);
-			dialStartloc = gameObject.transform.rotation.y;
+	void Update () 
+	{
+			
+		if(!isFull)
+		{	
+			if (this.transform.localEulerAngles.y > dialEndloc) 
+			{
+				transform.Rotate (Vector3.down * changeRate * Time.deltaTime);
+				dialStartloc = gameObject.transform.rotation.y;
+			}
+			else
+			{
+				isFull = true;
+			}
 		}
+		
+		if(isFull && isMoving)
+		{
+			usageRate = Random.Range(1,3);
+			if(this.transform.localEulerAngles.y > dialStartloc)
+			{
+				this.transform.Rotate(Vector3.up * usageRate * Time.deltaTime);
+			}
+			else
+			{
+				print ("Crash your dead");
+			}
+		}
+		
 				
 	}
 }
