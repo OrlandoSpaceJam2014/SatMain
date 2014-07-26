90e6c391b76dea0394cb03623d3b82d2de130980
gamingod4u <mcman407@gmail.com>
2014-07-26 12:22:31 -0400

    Update

---
+++ Assets/Scripts/OrbitingObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbitingObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
