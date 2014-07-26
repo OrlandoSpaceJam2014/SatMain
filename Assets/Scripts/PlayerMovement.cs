a4a36318ebee8522b758f3117d84817fb06039e0
Scott Joslin <zephemus@gmail.com>
2014-07-26 09:23:27 -0400

    Hi
    I"m an asshole that don't know how to use gitHub being told what to do
    by someone with horrid grammer.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float playerThrust;
+	public float rotationSpeed;
+	public float maxThrust;
+	public float naturalDrift;
+
+	public bool forward;
+	public bool reverse;
+	public bool port;
+	public bool starboard;
+
+	// Use this for initialization
+	void Start () {
+		forward = false;
+		reverse = false;
+		port = false;
+		starboard = false;
+
+		rotationSpeed = 50;
+		naturalDrift = 1;
+		maxThrust = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W)) {
+			ForwardThrust ();
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			ReverseThrust ();
+		}
+		if (Input.GetKeyDown(KeyCode.A)) {
+			PortThrust ();
+		}
+		if (Input.GetKeyDown(KeyCode.D)) {
+			StarboardThrust ();
+		}
+		if (Input.GetMouseButton (0)) {
+			transform.Rotate ((Input.GetAxis ("Mouse X") * rotationSpeed * Time.deltaTime), 
+		                 	(Input.GetAxis ("Mouse Y") * rotationSpeed * Time.deltaTime), 
+		                 	0, Space.Self);
+		}
+	}
+
+	void ForwardThrust(){
+		if (playerThrust < maxThrust)
+			rigidbody.AddForce (Vector3.forward * 50);
+		else
+			rigidbody.AddForce (Vector3.forward);
+	}
+
+	void ReverseThrust(){
+		if (playerThrust < maxThrust)
+			rigidbody.AddForce (Vector3.back * 50);
+		else
+			rigidbody.AddForce (Vector3.back);
+	}
+
+	void PortThrust(){
+		if (playerThrust < maxThrust)
+			rigidbody.AddForce (Vector3.left * 50);
+		else
+			rigidbody.AddForce (Vector3.left);
+	}
+
+	void StarboardThrust(){
+		if (playerThrust < maxThrust)
+			rigidbody.AddForce (Vector3.right * 50);
+		else
+			rigidbody.AddForce (Vector3.right);
+	}
+}
