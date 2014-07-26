c34589ca4a14f09b4abfe9ec80b89bf20c406ef3
gamingod4u <mcman407@gmail.com>
2014-07-26 13:52:29 -0400

    Update

--- Assets/Scripts/OrbitingObject.cs
+++ Assets/Scripts/OrbitingObject.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OrbitingObject : MonoBehaviour {
+public class OrbitingObject : MonoBehaviour
+ {
+ 	private GameObject		m_goTargetObject;
+ 	private float 			m_fRotSpeed = 6.0f;
+ 	private float 			m_fMoveSpeed = 3.0f;
 
+
+	void Awake()
+	{
+		m_goTargetObject = GameObject.FindGameObjectWithTag("Player");
+	}
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		RotateObject();
 	}
+ 	
+ 	void RotateObject()
+ 	{
+		this.transform.Rotate(Vector3.right * m_fRotSpeed * Time.deltaTime);
+		this.transform.Rotate(Vector3.up * m_fRotSpeed * Time.deltaTime);
+		this.transform.Rotate(Vector3.forward * m_fRotSpeed * Time.deltaTime);
+		
+ 	}
+ 	
+ 	void MoveObject()
+ 	{
+ 		
+ 	}
 }
