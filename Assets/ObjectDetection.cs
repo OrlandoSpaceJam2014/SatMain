5a8302dcf59aef315aad5f89678c6a2fc9101fae
Scott Joslin <zephemus@gmail.com>
2014-07-27 01:24:19 -0400

    lol

--- Assets/ObjectDetection.cs
+++ Assets/ObjectDetection.cs
@@ -3,53 +3,31 @@ using System.Collections;
 
 public class ObjectDetection : MonoBehaviour {
 
-	public bool detection;
-	public bool carryObject;
 
-	public Ray detectRay;
-	public RaycastHit hitInfo;
-	public GameObject objectScript;
-
-	public float rayLength = 500.0f;
-	private GrabObjectScript gbScript1;
-	private GrabObjectScript gbScript2;
-	private GrabObjectScript gbScript3;
+	private GameObject dish;
+	private GameObject panel;
+	private GameObject power;
 
 	// Use this for initialization
 	void Start () {
-		detection = false;
-		carryObject = false;
-		gbScript1 = GameObject.Find("Dish").GetComponent<GrabObjectScript> ();
-		gbScript2 = GameObject.Find("Panel").GetComponent<GrabObjectScript> ();
-		gbScript3 = GameObject.Find("Power").GetComponent<GrabObjectScript> ();
-		detectRay = new Ray (transform.position, transform.forward);
+		dish = GameObject.Find ("Dish");
+		panel = GameObject.Find ("Panel");
+		power = GameObject.Find ("Power");
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		Debug.DrawRay (detectRay.origin, detectRay.direction * rayLength);
-
-		if (Physics.Raycast (detectRay.origin, detectRay.direction, out hitInfo, rayLength, 0)) {
-			if(hitInfo.collider.tag == "Grabable" && Input.GetButtonDown("button 0")){
-				if(hitInfo.transform.gameObject.name == "Dish")
-					gbScript1.isGrabbed = true;
-				else if(hitInfo.transform.gameObject.name == "Panel")
-					gbScript1.isGrabbed = true;
-				else if(hitInfo.transform.gameObject.name == "Power")
-					gbScript3.isGrabbed = true;
-
-
-				GrabObject();
-			}
-			detection = true;
-			Debug.Log ("We hit something!");
-		}
-		else
-			detection = false;
 	}
 
-	void GrabObject(){
-		carryObject = true;
+	void OnTriggerEnter(Collider other){
+		if (other.name == "Dish")
+			Debug.Log ("Ran into the Dish");
+		else if (other.name == "Panel")
+			Debug.Log ("Ran into the Panel");
+		else if (other.name == "Power")
+			Debug.Log ("Ran into the Power");
 	}
 }
