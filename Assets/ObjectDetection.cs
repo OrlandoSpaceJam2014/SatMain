2dba281d7ca2a97dcf3f81262157d2794a14b91e
Scott Joslin <zephemus@gmail.com>
2014-07-27 02:39:51 -0400

    LateNight
    and I'm not done.

--- Assets/ObjectDetection.cs
+++ Assets/ObjectDetection.cs
@@ -8,12 +8,17 @@ public class ObjectDetection : MonoBehaviour {
 	private GameObject panel;
 	private GameObject power;
 
+	private static GrabObjectScript dishScript;
+	private static GrabObjectScript panelScript;
+	private static GrabObjectScript powerScript;
+
 	// Use this for initialization
 	void Start () {
 		dish = GameObject.Find ("Dish");
 		panel = GameObject.Find ("Panel");
 		power = GameObject.Find ("Power");
 
+		dishScript = 
 
 	}
 	
@@ -23,8 +28,10 @@ public class ObjectDetection : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other){
-		if (other.name == "Dish")
+		if (other.name == "Dish" && Input.GetButtonDown (0)) {
+
 			Debug.Log ("Ran into the Dish");
+		}
 		else if (other.name == "Panel")
 			Debug.Log ("Ran into the Panel");
 		else if (other.name == "Power")
