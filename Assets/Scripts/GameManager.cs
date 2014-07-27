fa9995e01f98bbc4a42749fa22842a87a5494015
gamingod4u <mcman407@gmail.com>
2014-07-27 03:54:46 -0400

    Done for the night

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance ;
+
+	public bool []			  Checkpoints;
+
+	public bool				  m_bIsDead = false;
+	// Use this for initialization
+	void Start ()
+	{	
+		Checkpoints = new bool[3];
+		for (int i = 0; i < 3; i++) 
+		{
+			Checkpoints[i] = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		for (int i = 0; i < 3; i++) 
+		{
+			if(!Checkpoints[i])
+			{
+				return;
+			}
+			else if( i == 3)
+			{
+				//end the game	you won.
+			}
+		}
+
+
+		if(m_bIsDead)
+		{
+			// end the game u died;
+		}
+	}
+}
---
+++ Assets/Scripts/PowerSupplyScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerSupplyScript : MonoBehaviour
+{
+
+	public GameObject 		m_goHiddenObject;
+	public GameObject		m_goParticle;
+	private bool 	  		m_bIsAttached = false;
+	
+	private GameManager 	m_cGameManager;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		m_cGameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(m_bIsAttached)
+		{
+			for (int i = 0; i < 3; i++) {
+				if(!m_cGameManager.Checkpoints[i])
+					m_cGameManager.Checkpoints[i] = true;
+			}
+		}
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if(other.name == "PowerCell")
+		{
+			m_bIsAttached = true;
+			DestroyObject(other.gameObject);
+			DestroyObject(m_goParticle.gameObject);
+			m_goHiddenObject.SetActive(true);
+		}
+	}
+}
