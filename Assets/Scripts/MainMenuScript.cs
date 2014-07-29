5d6296717e8285450c25155d918a6f014111095a
gamingod4u <mcman407@gmail.com>
2014-07-29 19:59:23 -0400

    i[date

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -4,17 +4,27 @@ using System.Collections;
 public class MainMenuScript : MonoBehaviour 
 {
 
+	private RaycastHit 	m_rcHit;			// collects info that the raycast hits.
+	private Ray 		m_rRay;				// ray for shooting in screen place on touch
 
-
-	// Use this for initialization
-	void Start () 
-	{
-	
-	}
-	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		if(Input.GetMouseButtonDown(0))  // if get mouse down or touch count != 0 same call 
+		{
+			m_rRay = Camera.main.ScreenPointToRay(Input.mousePosition); // set the ray at that position to world space
+			if(Physics.Raycast(m_rRay,out m_rcHit)) 	// if it hit something collect that data
+			{
+				HitReturn(m_rcHit.transform.name);      // call to switch collected data with response
+			}
+		}
+	}
+
+	private void HitReturn(string hitName)
+	{
+		if(hitName == "Start")  
+		{
+				Application.LoadLevel(1);			
+		}		
 	}
 }
